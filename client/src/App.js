416fda01f20beab4a738304c2ac667eb344e73c8
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-12-23 12:11:48 -0500

    resolved routes issue, all pages are connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Main from "./pages/Books";
-import Signup from "./pages/signup";
+import Main from "./pages/Mainpage";
+import Signup from "./pages/Signup";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-
+import Login from "./pages/Login";
 function App() {
   return (
     <Router>
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
-          <Route exact path="/dashboard" component={Dashboard} />
+          {/* <Route exact path="/dashboard" component={Dashboard} /> */}
           <Route component={NoMatch} />
         </Switch>
       </div>
