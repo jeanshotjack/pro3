a21fecbf3c576f15489e03cede258e5efd2f0cca
Mustafa Muhammad <m.muhammad43081@gmail.com>
2020-01-02 13:56:35 -0500

    photo change working, will change later, along with jumbotron

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,104 +1,29 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import API from "../utils/API";
-import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+import Slideshow from "../components/ImgSlide/Slideshow";
 // import Nav from "../components/Nav/Navbar";
 
+
 class Main extends Component {
-  state = {
-    title: "",
-    author: "",
-    flag: false,
-  };
-
-  componentDidMount() {
-    this.getPosts();
-  }
-
-  getPosts = () => {
-    API.getPosts()
-      .then(res =>
-        this.setState({ notes: res.data, title: "", author: "", flag: false, })
-      )
-      .catch(err => console.log(err));
-  };
-
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   if (this.state.title && this.state.author) {
-  //     API.saveBook({
-  //       title: this.state.title,
-  //       author: this.state.author,
-  //       synopsis: this.state.synopsis
-  //     })
-  //       .then(res => this.loadBooks())
-  //       .catch(err => console.log(err));
-  //   }
-  // };
 
   render() {
     return (
       <Container fluid>
         <Row>
-          <Col size="md-6">
+          <Col size="md-12">
             <Jumbotron>
               <h1>NOBIS</h1>
             </Jumbotron>
-            {/* <form>
-              <Input
-                value={this.state.title}
-                onChange={this.handleInputChange}
-                name="title"
-                placeholder="Title (required)"
-              />
-              <Input
-                value={this.state.author}
-                onChange={this.handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                value={this.state.synopsis}
-                onChange={this.handleInputChange}
-                name="synopsis"
-                placeholder="Synopsis (Optional)"
-              />
-              <FormBtn
-                disabled={!(this.state.author && this.state.title)}
-                onClick={this.handleFormSubmit}
-              >
-                Submit Book
-              </FormBtn>
-            </form> */}
+            <Slideshow />
           </Col>
-          {/* <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Books On My List</h1>
-            </Jumbotron>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col> */}
         </Row>
       </Container>
+      
     );
+
+
+
   }
 }
 
