9f681f31135c4e1799659f93e8489dc5c763aa57
easonchang15530 <eason15530@gmail.com>
2020-01-06 12:15:04 -0500

    add Main.js layout

---
+++ client/src/components/SideBar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import SubmitPost from "../SubmitPost";
+import { ListItem } from "../List";
+import { Row, Col } from "../Grid";
+
+import "./style.css";
+
+function SideBar(props) {
+
+
+}
+
+export default SideBar;
