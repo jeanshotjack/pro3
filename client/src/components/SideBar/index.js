d651ba2f923488dcd58c932b42d3636ef269bd0f
jeanshotjack <dshalenkova@gmail.com>
2020-01-06 12:47:47 -0500

    new post class

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -1,13 +1,21 @@
 import React from "react";
-// import SubmitPost from "../SubmitPost";
+import SubmitPost from "../SubmitPost";
 import { ListItem } from "../List";
 import { Row, Col } from "../Grid";
+import axios from "axios";
+import { PostsListItem } from "../Newsfeed";
 
 import "./style.css";
 
-function SideBar(props) {
+class NewPost extends Component {
+    state = {
+        title: title,
+        body: body
+    };
+SideBar(props) {
 
 
+}
 }
 
 export default SideBar;
