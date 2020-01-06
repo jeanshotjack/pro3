7fd28ccef1c4f1fe53fd81ef0ba3716ccaae4d25
jeanshotjack <dshalenkova@gmail.com>
2020-01-06 11:14:08 -0500

    post list item

---
+++ client/src/components/Newsfeed/PostsList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Row, Col } from "../Grid";
+
+
+// BookList renders a bootstrap list item
+export function PostsList({children}) {
+  return (
+    <ul className="list-group">{children}</ul>
+    );
+}
+
+  export class PostsListItem extends React.Component{
+
+    render(){
+     console.log(this.props)
+;    return (
+      <li>
+        <Container>
+          <Row>
+
+            <Col size="xs-8 sm-9">
+              <h3>{this.props.title}<span><h5><a target="_blank" href={this.props.link}>{this.props.username}</a></h5></span></h3>
+              <p>
+                {this.props.body}
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </li>
+    );
+    }
+  }
