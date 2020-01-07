2087cc9a603fd02ff279a2215d01a2d992be8f34
jeanshotjack <dshalenkova@gmail.com>
2020-01-07 14:14:13 -0500

    wahtever

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -3,111 +3,46 @@ import { ListItem } from "../List";
 import { Row, Col } from "../Grid";
 import SubmitPost from "../SubmitPost";
 import axios from "axios";
+import SideBar from "./sidebar";
 
 import "./style.css";
+// import console = require("console");
 
+class PostForm extends React.Component {
+    constructor() {
+        super();
+            this.state = {
+                postTitle: "",
+                postBody: "",
+            }
+        }
+    handleTitleInput = (event) => {
+        event.preventDefault();
+        this.setState({ postTitle: event.target.value })
+    }
+    handleBodyInput = (event) => {
+        event.preventDefault();
+        this.setState({ postBody: event.target.value })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
 
-
-function SideBar(props) {
-  return (
-    <ListItem>
-      <Row className="flex-wrap-reverse">
-        <Col size="md-8">
-<<<<<<< HEAD
-          <p>
-            Hello! {props.username}
-          </p>
-          <p >
-            Pronouns: {props.pronouns}
-          </p>
-          <p >
-            User Name: {props.username}
-          </p>
-          <p >
-            Social Media: {props.social}
-          </p>
-          
-          <div className="form-group">
-            <label for="post-text">New Post</label>
-            <input type="text" id={post.title} className="form-control" aria-describedby="post-text" placeholder="*Title*" />
-          </div>
-
-          <div className="form-group">
-            <label for="post-description">Post</label>
-            <textarea className="form-control" id={post.body} aria-describedby="post-description"
-              placeholder="*Feelings*"></textarea>
-          </div>
-          <button className="primary" onClick={() => 
-            {this.postToDB(props)}
-            }>
-            Submit
-        </button>
-=======
-
-          {/* <div class="sidenav">
-            <p className="font-main">
-              Hello! (Tom){props.userName}
-            </p>
-            <p className="font-main">
-              Profile: {props.userProfile}
-            </p>
-            <p className="font-main">
-              Pronoun: {props.userPronoun}
-            </p>
-            <p className="font-main">
-              User Name: {props.userName}
-            </p>
-            <p className="font-main">
-              Social Media: {props.userSocialMedia}
-            </p>
-
-
-            <div className="form-group">
-              <label for="post-text"><medium className="bolder-text">New Post</medium></label>
-              <input type="text" id="post-text" className="form-control" aria-describedby="post-text" placeholder="*Title*" />
-            </div>
-
-            <div className="form-group">
-              <label for="post-description">Post</label>
-              <textarea className="form-control" id="post-description" aria-describedby="post-description"
-                placeholder="*Feelings*"></textarea>
-            </div>
-          </div> */}
-
-
-          <div class="sidenav">
-            <p className="font-main">
-              Hello! (Tom){props.username}
-            </p>
-            {/* <p className="font-main">
-            Profile: {props.userprofile}
-          </p> */}
-            <p className="font-main">
-              Pronouns: {props.pronouns}
-            </p>
-            <p className="font-main">
-              User Name: {props.username}
-            </p>
-            <p className="font-main">
-              Social Media: {props.social}
-            </p>
-            <div className="form-group">
-              <label for="post-text"><h4 className="bolder-text">New Post</h4></label>
-              <input type="text" id="post-text" className="form-control" aria-describedby="post-text" placeholder="*Title*" />
-            </div>
-
-            <div className="form-group">
-              <label for="post-description">Post</label>
-              <textarea className="form-control" id="post-description" aria-describedby="post-description"
-                placeholder="*Feelings*"></textarea>
-            </div>
-          </div>
-
->>>>>>> 66b4b3af81bc16fcd0d9e8c11090e2ef6a5700ee
-        </Col>
-      </Row>
-    </ListItem>
-  );
+        console.log("Submitted!")
+        // const dbPost = new Post(event.target);
+        // axios.post("/api/posts", dbPost)
+        //     .catch(err => console.log(err))
+    }
+    
+render () {
+    return (
+    <SideBar handleTitle = {this.handleTitleInput} handleBody = {this.handleBodyInput} handleSubmit={this.handleSubmit}/>
+    );
 }
+}
+export default PostForm;
 
-export default SideBar;
+
+
+
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// Not sure what this is but I tried to compile without it and then added it back in and it works now
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 
