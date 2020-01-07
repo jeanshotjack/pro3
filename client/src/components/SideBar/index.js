b2253e233dee2caa7adc74d3e91e568630ae5a89
jeanshotjack <dshalenkova@gmail.com>
2020-01-07 11:40:02 -0500

    pull

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -41,33 +41,9 @@ function SideBar(props) {
             </div>
           </div>
 
+            <SubmitPost> </SubmitPost>
 
-          <p className="font-main">
-            Hello! {props.username}
-          </p>
-          {/* <p className="font-main">
-            Profile: {props.userprofile}
-          </p> */}
-          <p className="font-main">
-            Pronouns: {props.pronouns}
-          </p>
-          <p className="font-main">
-            User Name: {props.username}
-          </p>
-          <p className="font-main">
-            Social Media: {props.social}
-          </p>
-          <div className="form-group">
-            <label for="post-text">New Post</label>
-            <input type="text" id="post-text" className="form-control" aria-describedby="post-text" placeholder="*Title*" />
-          </div>
-
-          <div className="form-group">
-            <label for="post-description">Post</label>
-            <textarea className="form-control" id="post-description" aria-describedby="post-description"
-              placeholder="*Feelings*"></textarea>
-          </div>
-          <SubmitPost></SubmitPost>
+          
         </Col>
       </Row>
     </ListItem >
