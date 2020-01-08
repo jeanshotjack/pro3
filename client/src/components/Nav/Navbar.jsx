f0d9dd149f929162222ab80c6a0cc9d1b281e51c
Mustafa Muhammad <m.muhammad43081@gmail.com>
2020-01-08 15:36:26 -0500

    fixed issues with text box, rules page added

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Button, NavDropdown } from 'react-bootstrap';
+import { Navbar, Button, Dropdown } from 'react-bootstrap';
 import "../CSS/General.css";
 
 
@@ -9,43 +9,38 @@ function NavBar(props) {
   return (
 
     <div>
-      <Navbar fluid className="navbar fixed-top ">
+      <Navbar className="navbar fixed-top ">
         <Navbar.Brand href="/">
           <a href="/"><img className="noxNav" href="/home" src={require("../Nav/Nox.png")} /></a>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <Navbar.Collapse>
+        <Navbar.Collapse id="basic-navbar-nav">
 
-          <NavDropdown className="dropDown secondary"
-            eventKey={3}
-            title="Menu"
-          >
 
-            <Button href="/" className="dropdown-item btn-primary login">Home</Button>
+          <Dropdown className="dropMenu ml-auto">
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              Menu
+  </Dropdown.Toggle>
 
-            <Button href="/login" className="dropdown-item btn-primary login">Login</Button>
+            <Dropdown.Menu>
 
-            <Button href="/signup" className="dropdown-item btn-primary signup">Sign Up</Button>
+              <Button href="/" className="dropdown-item btn-primary login">Home</Button>
 
-            <Button href="/chat" className="dropdown-item btn-primary chat">Chat</Button>
+              <Button href="/login" className="dropdown-item btn-primary login">Login</Button>
 
-            <Button href="/logout" className="dropdown-item btn-primary logout">Logout</Button>
+              <Button href="/signup" className="dropdown-item btn-primary signup">Sign Up</Button>
 
-          </NavDropdown>
+              <Button href="/chat" className="dropdown-item btn-primary chat">Chat</Button>
 
+              <Button href="/logout" className="dropdown-item btn-primary logout">Logout</Button>
+
+              <Button href="/rules" className="dropdown-item btn-primary logout">Rules</Button>
+
+            </Dropdown.Menu>
+          </Dropdown>
         </Navbar.Collapse>
+
       </Navbar>
-
-      {/* <div>
-        <div className="container-fluid">
-          <div className="row justify-content-center">
-            <img className="noxLogo" src={require("../NoxLogo/Nox2.png")} />
-          </div>
-        </div>
-      </div> */}
-
-
 
     </div>
 
